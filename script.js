f79bd6d1eaeb0d008c3f13fc72a45ccb425df3de
BramGit12 <125472993+BramGit12@users.noreply.github.com>
2023-06-16 11:38:01 +0200

    purge2.0

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
-window.onload = OnLoad;
-function OnLoad() {
-    setTimeout(RemoveClass, 2600);
-}
+// window.onload = OnLoad;
+// function OnLoad() {
+//     setTimeout(RemoveClass, 2600);
+// }
 
-function RemoveClass() {
-    if (document.getElementById("head").classList == "moveoutup") {
-        document.getElementById("head").classList = "moveoutup";
-        // document.getElementById("contact").classList = "contactexit";
-        return;
-    }
-    // document.getElementById("contact").classList = "";
-    document.getElementById("head").classList = "";
-}
+// function RemoveClass() {
+//     if (document.getElementById("head").classList == "moveoutup") {
+//         document.getElementById("head").classList = "moveoutup";
+//         return;
+//     }
+//     document.getElementById("head").classList = "";
+// }
 
 let selected = null;
-let scrolling = false;
-let currentcard = null;
 function ClickLeft() {
-    if (scrolling == false) {
-        selected = "music";
-        document.getElementById("bgright").classList.add("unselected");
-        document.getElementById("bgleft").classList.add("selected");
-        document.getElementById("subtitle").innerHTML = "Music";
-        document.getElementById("head").classList = "moveoutup";
-        document.getElementById("musiccard").classList.add("fadeout");
-        document.getElementById("continueleft").classList.add("fadeout");
-    }
+    selected = "music";
+    document.getElementById("bgright").classList.add("unselected");
+    document.getElementById("bgright").classList.add("nohover");
+    document.getElementById("bgleft").classList.add("selected");
+    document.getElementById("bgleft").classList.add("nohover");
+    document.getElementById("subtitle").innerHTML = "Music";
+    // document.getElementById("head").classList = "moveoutup";
+    document.getElementById("musiccard").classList.add("fadeout");
+    document.getElementById("continueleft").classList.add("fadeout");
+
 }
 function ClickRight() {
-    if (scrolling == false) {
-        selected = "coding";
-        document.getElementById("bgleft").classList.add("unselected");
-        document.getElementById("bgright").classList.add("selected");
-        document.getElementById("subtitle").innerHTML = "Coding & Design";
-        document.getElementById("head").classList = "moveoutup";
-        document.getElementById("codingcard").classList.add("fadeout");
-        document.getElementById("continueright").classList.add("fadeout");
-    }
+    selected = "coding";
+    document.getElementById("bgleft").classList.add("unselected");
+    document.getElementById("bgright").classList.add("nohover");
+    document.getElementById("bgleft").classList.add("unselected");
+    document.getElementById("bgright").classList.add("nohover");
+    document.getElementById("subtitle").innerHTML = "Coding & Design";
+    // document.getElementById("head").classList = "moveoutup";
+    document.getElementById("codingcard").classList.add("fadeout");
+    document.getElementById("continueright").classList.add("fadeout");
 }
 function Expand() {
     if (document.getElementById("menuicon").classList == "open") {
